37a5677525280a19d7356eab734b2ae0240e602d
derrek-gass <dardcore@gmail.com>
2021-06-05 19:34:16 -0700

    updated first test project

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,23 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(def words (str "Inconvenienced"))
+(def words2 (str "dead"))
+
+(defn error-message [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+       (if (= severity :mild)
+         words words2)))
+
+(defn train
+  []
+  (println "Choo Choo!"))
+
+(defn -main [args]
+  (error-message args))
+
+(defn too-enthusiastic
+  "Return a cheer that might be a bit too enthusiastic"
+  [name]
+  (str "OH. MY. GOD! " name " YOU ARE MOST DEFINITELY THE MOST "
+       "AMAZING WOMAN EVER! I LOVE YOU SO MUCH!"))
